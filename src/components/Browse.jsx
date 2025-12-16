0e04dae05fb27e6e7ac3d1da3f65707668c5711a
Muzammil Momin <mominmuzammil157@gmail.com>
2025-12-16 23:32:51 +0530

    create movieSlice, fetch now playing movies from TMDB & add it to the redux store

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { API_OPTIONS } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import { addNowPlayingMovies } from '../utils/moviesSlice'
 
 const Browse = () => {
+
+  const dispatch = useDispatch()
+
+  const getNowPlayingMovies = async () => {
+    const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS)
+    const data = await response.json()
+    dispatch(addNowPlayingMovies(data.results))
+  }
+
+  useEffect(() => {
+    getNowPlayingMovies()
+  }, [])
+
   return (
     <div>
       <Header />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,9 @@
 export const USER_AVATAR = '/public/assets/user-profile-icon.jpg';
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDk0MzIyMzMwZjExNTA1Yzg4ZTRlYzQzZWUwYzhiZiIsIm5iZiI6MTc2NTkwNjY0My43NjQsInN1YiI6IjY5NDE5OGQzOTEwYzY5MjE2MmIyNDdkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.K9ZCPivw-IgiG0YmV-49GwmH3ezHhf3hHid-I6kgE7o'
+  }
+};
