d9245ead1f88215db8ee850c5cf7421849daebbd
Muzammil Momin <mominmuzammil157@gmail.com>
2025-12-19 19:34:50 +0530

    seperate out fetch trailer code to custom hook

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,36 +1,18 @@
-import React, { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailer } from "../utils/moviesSlice";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
-  const disptach = useDispatch();
   const trailerVideo = useSelector((store) => store.movies.trailer);
-
-  const getMovieTrailer = async () => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
-      API_OPTIONS
-    );
-    const data = await response.json();
-    const filterData = data.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : data.results[0];
-
-    disptach(addTrailer(trailer));
-  };
-
-  useEffect(() => {
-    getMovieTrailer();
-  }, []);
+  useMovieTrailer(movieId);
 
   return (
-    <div>
+    <div className="w-screen">
       <iframe
-        width="560"
-        height="315"
-        src={`https://www.youtube.com/embed/${trailerVideo?.key}`}
+        className="w-screen aspect-video"
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1&loop=1&controls=0&rel=0&&showinfo=1`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        
       ></iframe>
     </div>
   );
