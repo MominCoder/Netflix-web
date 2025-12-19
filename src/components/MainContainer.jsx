0432b77c742c1719fedba898d1469f8524ac367d
Muzammil Momin <mominmuzammil157@gmail.com>
2025-12-19 19:34:08 +0530

    CSS changes

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -12,7 +12,7 @@ const MainContainer = () => {
   const {title, overview, id} = mainMovie
 
   return (
-    <div className="">
+    <div className="relative">
       <VideoBackground movieId = {id} />
       <VideoTitle title={title} overview={overview} />
     </div>
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-
-const VideoTitle = ({title, overview}) => {
+const VideoTitle = ({ title, overview }) => {
   return (
-    <div className='text-white bg-gradient-to-r from-black px-5'>
-        <h1 className='text-xl md:text-2xl font-bold'>{title}</h1>
-        <p className='hidden md:inline-block py-6 text-lg w-1/4'>{overview}</p>
+    <div className="text-white bg-gradient-to-r from-black px-5 absolute top-0 left-0 z-10 h-full flex items-center">
+      <div className="">
+        <h1 className="text-xl md:text-2xl font-bold">{title}</h1>
+        <p className="hidden md:inline-block py-6 text-lg w-1/2">{overview}</p>
         <div>
-            <button className='bg-white text-black py-1 md:py-4 px-3 md:px-12 text-xl rounded-lg hover:bg-opacity-80 cursor-pointer'>Play</button>
-            <button className='hidden md:inline-block mx-2  bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg cursor-pointer'>More Info</button>
+          <button className="hover:bg-opacity-50 bg-white text-black py-2.5 px-4 text-xl rounded-lg cursor-pointer">Play</button>
+          <button className="hidden md:inline-block mx-2 bg-gray-500 text-white py-2.5 px-4 text-xl hover:bg-opacity-50 rounded-lg cursor-pointer">More Info</button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default VideoTitle
+export default VideoTitle;
