027b3aafeae9f013151525b3665752dd0a76934a
Muzammil Momin <mominmuzammil157@gmail.com>
2025-12-25 01:09:09 +0530

    fix css

--- src/components/GptSearchSuggestion.jsx
+++ src/components/GptSearchSuggestion.jsx
@@ -4,7 +4,7 @@ import MovieList from "./MovieList"
 const GptSearchSuggestion = () => {
   const searchResults = useSelector((store) => store.gpt.searchResults); 
   
-  if (!searchResults) return <p>Please search only movies</p>;
+  if (!searchResults) return <p className="text-white text-center text-md">**Please search only movies**</p>;
 
   return (
     <div className="p-4 m-4 bg-black text-white bg-opacity-90">
