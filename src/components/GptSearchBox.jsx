2ef99b7cf672db2bce5fba9aa3c0ae36911e4807
Muzammil Momin <mominmuzammil157@gmail.com>
2025-12-25 01:08:55 +0530

    fix: gpt search page background image path

--- src/components/GptSearchBox.jsx
+++ src/components/GptSearchBox.jsx
@@ -7,11 +7,11 @@ const GptSearchBox = () => {
       <figure className="fixed z-10">
         <img
           className="h-screen object-cover"
-          src={"/public/assets/Login_BG_large.jpg"}
+          src="/assets/Login_BG_large.jpg"
           alt="logo"
         />
       </figure>
-      <div className="absolute left-0 top-1/2 z-30 w-full">
+      <div className="absolute left-0 top-2/6 z-30 w-full">
         <GptSearchInput />
         <GptSearchSuggestion />
       </div>
