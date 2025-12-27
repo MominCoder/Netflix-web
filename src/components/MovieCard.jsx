1ead9cca239e67ddfb89a5684c91a88387d7ef61
Muzammil Momin <mominmuzammil157@gmail.com>
2025-12-28 01:34:07 +0530

    change cursor to pointer for links

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,8 @@ import { IMG_CDN_URL } from "../utils/constants"
 
 const MovieCard = ({posterPath, title}) => {
   return (
-    <figure className="md:w-48 w-32">
-        <img className="w-[100%]" src={IMG_CDN_URL+posterPath} alt={title} />
+    <figure className="md:w-48 w-32 cursor-pointer">
+        <img className="w-[100%]" src={IMG_CDN_URL+posterPath} alt={title} draggable={false} />
     </figure>
   )
 }
