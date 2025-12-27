6b5f13ee70b2f00fbe5b187a1f9b8f123ce73ec5
Muzammil Momin <mominmuzammil157@gmail.com>
2025-12-27 17:28:40 +0530

    fix: done memoization to prevent recall of fetch API for movie & trailer data

--- src/hooks/useFetchMovies.js
+++ src/hooks/useFetchMovies.js
@@ -1,25 +1,26 @@
-import { useDispatch, useSelector } from "react-redux"
-import { useEffect } from "react"
-import { API_OPTIONS } from "../utils/constants"
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
 
 const useFetchMovies = (selector, action, type) => {
   const dispatch = useDispatch();
   const movies = useSelector(selector);
 
+  const fetchMovies = async () => {
+    try {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/${type}?page=1`,
+        API_OPTIONS
+      );
+      const json = await res.json();
+      dispatch(action(json.results));
+    } catch (error) {
+      console.error("Failed to fetch movies:", error);
+    }
+  };
+
   useEffect(() => {
-    if (movies && movies.length > 0) return;
-
-    const fetchMovies = async () => {
-      try {
-        const res = await fetch(`https://api.themoviedb.org/3/movie/${type}?page=1`, API_OPTIONS);
-        const json = await res.json();
-        dispatch(action(json.results));
-      } catch (error) {
-        console.error("Failed to fetch movies:", error);
-      }
-    };
-
-    fetchMovies();
+    !movies && fetchMovies();
   }, []);
 };
 
--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,14 +1,13 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addTrailer } from "../utils/moviesSlice";
 import { useEffect } from "react";
 
 const useMovieTrailer = (movieId) => {
-  // fetch trailer video data & update store with it
-
   const disptach = useDispatch();
+  const trailer = useSelector(store => store.movies.trailer)
 
-  const getMovieTrailer = async () => {
+  const getMovieTrailer = async () => {    
     const response = await fetch(
       `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
       API_OPTIONS
@@ -21,7 +20,7 @@ const useMovieTrailer = (movieId) => {
   };
 
   useEffect(() => {
-    getMovieTrailer();
+    !trailer && getMovieTrailer();
   }, []);
 };
 
