29b71d1e372f4a1e8f59104575772664f3d63bbe
Muzammil Momin <mominmuzammil157@gmail.com>
2025-12-28 01:33:23 +0530

    fix: stretch BG image to full screen on GPT search page

--- src/components/GptSearchBox.jsx
+++ src/components/GptSearchBox.jsx
@@ -4,14 +4,14 @@ import GptSearchSuggestion from "./GptSearchSuggestion";
 const GptSearchBox = () => {
   return (
     <div className="relative h-screen">
-      <figure className="fixed z-10">
+      <figure className="fixed z-10 w-full">
         <img
-          className="h-screen object-cover"
+          className="h-screen object-cover w-full"
           src="/assets/Login_BG_large.jpg"
           alt="logo"
         />
       </figure>
-      <div className="absolute left-0 top-2/6 z-30 w-full">
+      <div className="absolute left-0 top-2/12 z-30 w-full">
         <GptSearchInput />
         <GptSearchSuggestion />
       </div>
